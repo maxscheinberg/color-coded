8a1bfb24f2ba449a881e2feb94516b1a6b741970
maxscheinberg <75459032+maxscheinberg@users.noreply.github.com>
2026-04-15 14:51:28 -0400

    teleporter animation autoplays

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,19 +1,15 @@
 extends Control
- 
- 
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_01.tscn")
- 
- 
+
 func _on_level_select_pressed() -> void:
 	# Hook this up later when you have a level select screen
 	pass
- 
- 
+
 func _on_settings_pressed() -> void:
 	# Hook this up later when you have a settings screen
 	pass
- 
- 
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
